3a05ea798db5e8647e215accb582ee7c68e35a7b
DominikaGit <dominika.pawl@gmail.com>
2022-12-17 11:24:46 +0100

    poczatek programu

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Program ma tworzyc rozne wzory z cyfr
+
+int main() {
+    int i, j, k; //k- rzedy
+    printf("Wpisz liczbe rzedow: \n");
+    scanf("%d", &k);
+
+    //jaka petla dla polpiramidy?
+
+    return 0;
+}
